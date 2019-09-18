0a561f29e285bbeaf10de147ccca5b5ae70ef115
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-09-18 18:33:19 +0530

    paddle is done

--- script.js
+++ script.js
@@ -5,8 +5,15 @@ let ballXPosition=canvas.width/2;
 let ballYPosition= canvas.height-30;
 let ballXSpeed=2;
 let ballYSpeed=-3;
+let paddleHeight=10;
+let paddleWidth=50;
+let paddleSize=(canvas.width-paddleWidth)/2;
+var rightKeyPressed=false
+let leftKeyPressed=false
 
 window.onload=function(){
+    document.addEventListener("keydown", keyIsPressed,false )
+    // document.addEventListener("keyup", keyIsReleased,false )
     setInterval(clearCanvas,10)
 }
 
@@ -16,7 +23,11 @@ function drawBall() {
     canvasContext.arc(ballXPosition,ballYPosition, ballRadius, 0, 2 * Math.PI);
     canvasContext.fill();
     canvasContext.closePath();
-   
+
+    canvasContext.beginPath();
+    canvasContext.fillStyle='#ffffff';
+    canvasContext.fillRect(paddleSize,canvas.height-paddleHeight,paddleWidth,paddleHeight);
+    canvasContext.closePath();
 }
 
 function clearCanvas(){
@@ -31,7 +42,20 @@ function clearCanvas(){
     ballYPosition=ballYPosition+ballYSpeed;
     if((ballYPosition>canvas.height)||(ballYPosition<0)){
         ballYSpeed=-ballYSpeed
-    }
-    
+    }  
 }
+
+function keyIsPressed(evt){
+    if(evt.key=="ArrowRight"){
+        rightKeyPressed=true
+        console.log(rightKeyPressed)
+    }
+
+}
+
+console.log(rightKeyPressed)
+
+
+
+
 
