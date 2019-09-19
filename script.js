bb6c366fb0521e873cf884f0755c8842d4d8cb08
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-09-19 18:31:19 +0530

    brick is created

--- script.js
+++ script.js
@@ -12,11 +12,42 @@ var rightKeyPressed=false
 let leftKeyPressed=false
 let gameLife=3
 var interval=""
+let brickRowCount = 4;
+let brickColumnCount = 10;
+let brickWidth = 75;
+let brickHeight = 20;
+let brickPadding = 10;
+let brickOffsetTop = 30;
+let brickOffsetLeft = 45;
+
+let bricks = [];
+for(let column=0; column<brickColumnCount; column++) {
+    bricks[column] = [];
+    for(let row=0; row<brickRowCount; row++) {
+        bricks[column][row] = { x: 0, y: 0 };
+    }
+}
+
 
 window.onload=function(){
     document.addEventListener("keydown", keyIsPressed,false )
     document.addEventListener("keyup", keyIsReleased,false )
     interval = setInterval(clearCanvas,10)
+}
+function drawBricks() {
+    for(let column=0; column<brickColumnCount; column++) {
+        for(let row=0; row<brickRowCount; row++) {
+            let brickX = (column*(brickWidth+brickPadding))+brickOffsetLeft;
+            let brickY = (row*(brickHeight+brickPadding))+brickOffsetTop;
+            bricks[column][row].x = brickX;
+            bricks[column][row].y = brickY;
+            canvasContext.beginPath();
+            canvasContext.fillStyle = "#0095DD";
+            canvasContext.rect(brickX, brickY, brickWidth, brickHeight);
+            canvasContext.fill();
+            canvasContext.closePath();
+        }
+    }
 }
 
 function drawBall() {
@@ -34,11 +65,22 @@ function drawCanvas(){
     canvasContext.fillRect(paddleSize,canvas.height-paddleHeight,paddleWidth,paddleHeight);
     canvasContext.closePath();
 }
-
+function collisionDetection() {
+    for(let column=0; column<brickColumnCount; column++) {
+        for(let row=0; row<brickRowCount; row++) {
+            let brickPosition = bricks[column][row];
+            if(ballXPosition > brickPosition.x && ballXPosition < brickPosition.x + brickWidth && ballYPosition > brickPosition.y && ballYPosition < brickPosition.y+brickHeight) {
+                ballYSpeed = -ballYSpeed;
+            }
+        }
+    }
+}
 function clearCanvas(){
     canvasContext.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+    drawBricks();
     drawCanvas();
+    collisionDetection();
     
     ballXPosition=ballXPosition+ballXSpeed;
     if((ballXPosition>canvas.width)||(ballXPosition<0)){
@@ -76,7 +118,6 @@ function keyIsPressed(evt){
     }
     if(evt.key=="ArrowLeft"){
         leftKeyPressed=true
-        console.log("arrow left working")   
     }
 }
 
